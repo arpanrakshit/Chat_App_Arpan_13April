d9081a7658296c350ea123e369452c934cdd4396
ARPAN RAKSHIT <122547958+arpanrakshit@users.noreply.github.com>
2024-04-13 13:31:43 +0530

    Add files via upload

---
+++ Chat-App/Chat-App/WebSocket/src/main/java/com/arpan/websocket/chat/MessageType.java
@@ -0,0 +1,11 @@
+package com.arpan.websocket.chat;
+
+public enum MessageType {
+	
+	
+	
+	CHAT,
+    JOIN,
+    LEAVE
+
+}
---
+++ Chat-App/Chat-App/WebSocket/src/main/java/com/arpan/websocket/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.arpan.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig {
+
+	
+	
+	
+	
+	
+}
